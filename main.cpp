a4e62579f7041064706ab3406d85e50016b076f5
Alex D. <kukalooka@icloud.com>
2025-06-11 18:19:03 +0200

    Cleared out main

--- main.cpp
+++ main.cpp
@@ -3,10 +3,6 @@
 using namespace std;
 
 int main() {
-    int licz2, licz5, licz6, licz3;
-    cin >> licz2 >> licz3 >> licz5 >> licz6;
-
-    cout << min(licz2, min(licz5, licz6)) * 256 + min(licz3, licz2 - min(licz2, min(licz5, licz6))) * 32 << endl;
 
     return 0;
 }
